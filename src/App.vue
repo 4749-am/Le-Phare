41bc659a7f650436bfe9ce27283af500c9cc9afc
git config --global user.name Anne-Marie <amm47home@gmail.com>
2025-08-21 10:58:35 +0200

    app template modified

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
+  <h1>Le Phare</h1>
+  <p>Une lumière qui vous guide dans l'océan de l'apprentissage en ligne.</p>
 </template>
 
 <style scoped></style>
